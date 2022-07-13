2f572f7330bc80e84f2ebcf93061b1035410c916
Luigito17 <98845278+Luigito17@users.noreply.github.com>
2022-07-13 14:32:38 -0500

    Add files via upload
    Esto es nuevo

---
+++ Proyecto Cardenas/js/script.js
@@ -0,0 +1,30 @@
+navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () =>{
+    navbar.classList.toggle('active');
+
+}
+
+profile = document.querySelector('.header .flex .profile');
+
+document.querySelector('#user-btn').onclick = () =>{
+    profile.classList.toggle('active');
+
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    profile.classList.remove('active');
+}
+
+function loader(){
+    document.querySelector('.loader').style.display = 'none';
+}
+
+// notice 1000 = 1 seconds
+// so 2000 = 2 seconds
+
+function fadeOut(){
+    setInterval(loader, 2000);
+}
+
+window.onload = fadeOut();
